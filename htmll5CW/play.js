41e2a9fdb01dc4156320f0b0d6d80dee4c1d5fd2
Itay-Arad <33072979+Itay-Arad@users.noreply.github.com>
2018-01-20 19:34:05 +0200

    Initial commit

---
+++ htmll5CW/play.js
@@ -0,0 +1,33 @@
+﻿function playVideo() {
+    var myVideo = document.getElementById("myVideo");
+    myVideo.play();
+}
+
+
+function changeColor() {
+    var myColor = document.getElementById("myColor");
+    var content = document.getElementById("content");
+    var color = myColor.value;
+    content.style.backgroundColor = color;
+}
+function showUserLocation() {
+    navigator.geolocation.getCurrentPosition(showLocation); //function inside function
+}
+
+function showLocation(location) {
+    var latitude = location.coords.latitude;
+    var longitude = location.coords.longitude;
+    alert(latitude + " , " + longitude);
+}
+
+//function saveData() {
+//    var myEmail = document.getElementById("myEmail");
+//    var email = myEmail.value;
+//    localStorage.setItem("userEmail", email);
+//}
+
+//function loadData() {
+//    var myEmail = document.getElementById("myEmail");
+//    var email = localStorage.getItem("userEmail");
+//    myEmail.value = email;
+//    }
